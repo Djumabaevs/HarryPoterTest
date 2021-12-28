8710e374de90ef65d7c250221dace26495b105ca
BakytDzhumabaev <djumabaevb@gmail.com>
2021-12-28 10:13:51 +0600

    Initial Commit

---
+++ HarryPoterTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPoterTest
+//
+//  Created by Bakyt Dzhumabaev on 28/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
